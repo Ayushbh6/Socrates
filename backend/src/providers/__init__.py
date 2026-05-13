a82fff528b1893171c83dbe0d3a77711acc4ecbf
ayushaibydna <ayush@aibydna.com>
2026-05-13 21:00:52 +0200

    Reset main for fresh start

--- backend/src/providers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .openai_adapter import OpenAIAdapter
-from .gemini_adapter import GeminiAdapter
-from .ollama_adapter import OllamaAdapter
-from .openrouter_adapter import OpenRouterAdapter
-
-__all__ = ["OpenAIAdapter", "GeminiAdapter", "OllamaAdapter", "OpenRouterAdapter"]
